b0406ef0e8c73090bd2d6d5ac7ff31b645b8c893
Renato Bispo <renatosilvabispo@outlook.com>
2023-04-09 00:45:49 -0300

    feat: type server cracking scripts args

--- src/scripts/server-cracking/get-crackable-servers.ts
+++ src/scripts/server-cracking/get-crackable-servers.ts
@@ -1,20 +1,21 @@
 import { NS } from '@ns';
 import { withResponse } from 'helpers/exec-and-wait-response';
 import { GetAllServersResponse } from 'types/server-discovery';
+import { ExtendedScriptArgs } from '/types/scripts';
+import { GetCrackableServersArgs } from '/types/server-cracking';
 
 export async function main(ns: NS): Promise<void> {
-  const [uuid, servers, availablePortOpenersQty] = ns.args;
+  const [uuid, servers, availablePortOpenersQty] =
+    ns.args as ExtendedScriptArgs<GetCrackableServersArgs>;
 
   await withResponse(uuid as string, async () => {
-    const parsedServers = JSON.parse(
-      servers as string
-    ) as GetAllServersResponse;
+    const parsedServers = JSON.parse(servers) as GetAllServersResponse;
 
     return parsedServers.filter((server) => {
       return (
         !server.purchasedByPlayer &&
         !server.hasAdminRights &&
-        (availablePortOpenersQty as number) >= server.numOpenPortsRequired
+        availablePortOpenersQty >= server.numOpenPortsRequired
       );
     });
   });
